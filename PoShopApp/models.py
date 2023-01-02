0b6d109ced7bec0d0114b0a78608d0677dc714f0
Mioshek <milosz.szczygiel@interia.pl>
2023-01-03 00:19:14 +0100

    html, models, urls, views displaying items

--- PoShopApp/models.py
+++ PoShopApp/models.py
@@ -6,97 +6,52 @@ from Users.models import Profile
 # Create your models here.
 
 
-class Fishing_rod(models.Model): 
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(10000)])
-    
-    def __str__(self) -> str:
-        return self.name
-    
-class Spinning_wheel(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-class Chair(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-class Natural_bait(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-class Crankbait(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-class Twister(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-class Rubber_bait(models.Model):
-    name = models.CharField(max_length=128)
-    price = models.PositiveIntegerField(default=0,
-                                        validators=[MinValueValidator(0),
-                                        MaxValueValidator(1000)])
-    
-    def __str__(self) -> str:
-        return self.name
-    
-    
+class EquipmentCategories(models.TextChoices):
+    FISHING_ROD = 'Fishing Rod'
+    SPINNING_WHEEL = 'Spinning Wheel'
+    CHAIR = 'Chair'
+    NATURAL_BAIT = 'Natural Bait'
+    CRANKBAIT = 'Crankbait'
+    TWISTER = 'Twister'
+    RUBBER_BAIT = 'Rubber Bait'
+    WOBBLERS = 'Wobblers'
+  
 class Equipment(models.Model):
-    class EquipmentCategories(models.TextChoices):
-        FISHING_ROD = Fishing_rod
-        SPINNING_WHEEL = Spinning_wheel
-        CHAIR = Chair
-        NATURAL_BAIT = Natural_bait
-        CRANKBAIT = Crankbait
-        TWISTER = Twister
-        RUBBER_BAIT = Rubber_bait
         
-    category_name = models.CharField(
+    category = models.CharField(
            max_length=50,
            choices=EquipmentCategories.choices
     )
-    brand = models.CharField(max_length=64)
+    brand = models.CharField(max_length=64, null=True, default='Null')
+    name = models.CharField(max_length=128)
+    price = models.FloatField(default=0,
+                                        validators=[MinValueValidator(0),
+                                        MaxValueValidator(1000)])
+    
+    def get_absolute_url(self):
+        return reverse("shop_list", kwargs={"pk": self.pk})
     
     def __str__(self) -> str:
-        return self.category_name
+        return self.name
     
 
 class Order(models.Model):
-    customer = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL)
-    price = models.PositiveIntegerField()
-    orderdate = timezone.now()
+    customer = models.ForeignKey(Profile, null=True, on_delete=models.PROTECT)
+    total_price = models.PositiveIntegerField(null=True)
+    orderdate = models.DateTimeField(blank=True, null=True)
     approved_order = models.BooleanField(default=False)
+    basket = models.ForeignKey('PoShopApp.Equipment', related_name='basket_items', on_delete=models.CASCADE, null=True)
     
-    # def get_absolute_url(self):
-    #     return reverse("model_detail", kwargs={"pk": self.pk})
-    def add_to_cart(self):
-        pass
+    def get_absolute_url(self):
+        return reverse("shop_list", kwargs={"pk": self.pk})
+    
+    def add_to_basket(self):
+        self.total_price = timezone.now()
     
     def approve_orders(self):
+        self.save()
         return self.filter(approved_order=True)
     
+    def __str__(self) -> str:
+        return self.customer.user.username
     
--- PoShopApp/urls.py
+++ PoShopApp/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'PoShopApp'
 
 urlpatterns = [
-    path('shop/', views.show_shop, name='show_shop'),
-    path('order/<int:pk>', views.order, name='order'),
+    path('', views.EquipmentListView.as_view(), name="shop_list"),
+    path('item/new/', views.CreateEquipmentView.as_view(), name='shop_new'),
+    path('item/<int:pk>/edit/', views.EquipmentUpdateView.as_view(), name='shop_edit'),
 ]
 
